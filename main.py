62f7bd8b638b689f9980a7a8c7c04a19b8e09f7c
Mustafas1998 <ms6534126@gmail.com>
2023-08-21 01:11:47 +0300

    Change 3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import datetime
 
-def data_transfer(directory):
+def data_transfer(directory): # Big 0notation is O(n)
     """This function transfer data from file and append them in a list of dictionaries"""
     #https://stackoverflow.com/questions/34976122/import-data-from-file-to-dictionary-in-python
     #using with statement to open a file in read mode.
     #https://www.geeksforgeeks.org/python-datetime-strptime-function/
-    #use this link to format date
+    #use this link to format date.
     employee_data = []
     with open(directory, 'r') as f:
         for line in f:
@@ -14,7 +14,7 @@ def data_transfer(directory):
             employee_data.append({'date': datetime.datetime.strptime(date, "%Y%m%d"), 'gender': gender, 'salary': int(salary), 'ID': ID, 'username': username})
     return employee_data
 
-def admin_menu():
+def admin_menu(): # Big 0notation is O(1)
     """Displays the menu of admin"""
     print("\nAdmin Menu:")
     print("1. Display Statistics")
@@ -25,16 +25,17 @@ def admin_menu():
     print("6. Raise Employee Salary")
     print("7. Exit")
 
-def employee_menu():
+def employee_menu(): # Big 0notation is O(1)
     """Display menu of employee"""
+    # Big 0notation is O(1)
     print("\nEmployee Menu:")
     print("1. Check Salary")
     print("2. Exit")
 
-def save(directory, employee_data):
+def save(directory, employee_data): # Big 0notation is O(n)
     """Saves the data edited by admin in Employees.txt file"""
     #https://www.w3schools.com/python/python_file_write.asp
-    #used with statement to open a file in write mode
+    #used with statement to open a file in write mode.
     with open(directory, 'w') as f:
         for data in employee_data:
             date = data['date'].strftime("%Y%m%d")
@@ -43,3 +44,11 @@ def save(directory, employee_data):
 
 
 
+def display_employees(employee_data): # Big 0notation is O(n log n)
+    """Displays all employees in system in ascending order of the joining date """
+    #https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
+    employee = sorted(employee_data, key=lambda x:x['date'])
+    #using sorted to sort the employees by the date of joining from oldest to newest.
+    for data in employee:
+        join_date = data['date'].strftime("%Y%m%d")
+        print(f"{data['ID']}, {data['username']}, {join_date}, {data['gender']}, {data['salary']}")
