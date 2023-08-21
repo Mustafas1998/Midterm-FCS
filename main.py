4cd58c6c63034165ddd87f1b738a5f590c9bfab9
Mustafas1998 <ms6534126@gmail.com>
2023-08-21 18:30:29 +0300

    Change 6

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-from collections import  Counter
+from collections import Counter
 def data_transfer(directory): # Big 0notation is O(n), n is the number of line in file.txt
     """This function transfer data from file and append them in a list of dictionaries"""
     #https://stackoverflow.com/questions/34976122/import-data-from-file-to-dictionary-in-python
@@ -11,7 +11,7 @@ def data_transfer(directory): # Big 0notation is O(n), n is the number of line i
         for line in f:
             ID, username, date, gender, salary = line.split(', ')
             #appending data into list of dictionaries:
-            employee_data.append({'date': datetime.datetime.strptime(date, "%Y%m%d"), 'gender': gender, 'salary': int(salary), 'ID': ID, 'username': username})
+            employee_data.append({'ID': ID, 'username': username, 'date': datetime.datetime.strptime(date, "%Y%m%d"), 'gender': gender, 'salary': int(salary)})
     return employee_data
 
 def admin_menu(): # Big 0notation is O(1)
@@ -40,16 +40,16 @@ def save(directory, employee_data): # Big 0notation is O(n), n is the number of 
         for data in employee_data:
             date = data['date'].strftime("%Y%m%d")
             #https://stackoverflow.com/questions/14619494/how-to-understand-strptime-vs-strftime
-            f.write(f"{data['ID']}, {data['salary']}, {date}, {data['username']}, {data['gender']}\n")
+            f.write(f"{data['ID']}, {data['username']}, {date}, {data['gender']}, {data['salary']}\n")
 
 def salary_raise(employee_data): # 0(n),n is number of employees
     """Raise the salary of an employee by percentage"""
     ID = input("Enter ID of the employee: ")
     #https://stackoverflow.com/questions/4260280/if-else-in-a-list-comprehension
     #used list comprehension to create True_employee.
-    True_employee = [employee for employee in employee_data if employee['ID'] == ID ]
-    if True_employee:
-        for e in True_employee:
+    Chosen = [employee for employee in employee_data if employee['ID'] == ID ]
+    if Chosen:
+        for e in Chosen:
             raise_percent = float(input("Enter percentage of the raise: "))
             e['salary'] = int(e['salary'] + (raise_percent / 100) * e['salary'])
             print("salary raised.")
@@ -57,7 +57,7 @@ def salary_raise(employee_data): # 0(n),n is number of employees
         print("No employee with such ID")
 
 
-def stats(employee_data):
+def stats(employee_data): # O(n) where n is the number of employees
     """Displays the number of male and female employees separately"""
     # https://stackoverflow.com/questions/37438550/how-can-i-count-occurrences-of-values-in-a-list-of-dicts
     # usage of Counter
@@ -66,6 +66,19 @@ def stats(employee_data):
         Gender[employee['gender']] += 1
     print(Gender)
 
+
+def remove_employee(employee_data): # O(n) where n is the number of employees
+    """Removes and employee from list of employees by asking for its ID"""
+    #https://stackoverflow.com/questions/4260280/if-else-in-a-list-comprehension
+    #list.remove(i)
+    ID = input("Enter ID of employee you want to remove: ")
+    Chosen = [employee for employee in employee_data if employee['ID'] == ID]
+    if Chosen:
+        for e in Chosen:
+            employee_data.remove(e)
+            print("Employee Removed")
+    else:
+        print("No employee with such ID!")
 
 def display_employees(employee_data): # Big 0notation is O(n log n), n is number of employees in employee_data
     """Displays all employees in system in ascending order of the joining date """
@@ -92,13 +105,13 @@ def Main(): # Big 0notation is O(n),n is number of employees in system
                 if choice == '1':
                     stats(employee_data)
                 elif choice == '2':
-
+                    print(1)
                 elif choice == '3':
                     display_employees(employee_data)
                 elif choice == '4':
-
+                    print(2)
                 elif choice == '5':
-
+                    remove_employee(employee_data)
                 elif choice == '6':
                     salary_raise(employee_data)
                 elif choice == '7':
@@ -128,9 +141,9 @@ def Main(): # Big 0notation is O(n),n is number of employees in system
                         #https://www.geeksforgeeks.org/python-now-function/ usage of now() function
                         #link in first function for hours, minutes and seconds formatting
                         Timestamp = datetime.datetime.now()
-                        formatted_timestamp = Timestamp.strftime("%Y%m%d %H:%M:$S")
-                        with open("Timestamp-login.txt", 'a') as f: #using with statement in append mode.
-                            f.write(f"{username}, {formatted_timestamp}")
+                        formatted_timestamp = Timestamp.strftime("%Y%m%d %H:%M:%S")
+                        with open("Timestamp-login.txt", 'a') as f:  # using with statement in append mode.
+                            f.write(f"{username}, {formatted_timestamp}\n")
                         break
                     else:
                         print("Please select a valid option!")
